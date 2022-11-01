b7662419da591f1a0cc3ca53502880b610b82ee2
Vadim Zhdanov <vadimir29@gmail.com>
2022-11-01 20:00:54 +0700

    Test commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("test");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter >
-  <Provider store={store}>
-  <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
