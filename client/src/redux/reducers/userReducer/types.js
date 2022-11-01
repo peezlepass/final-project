26b3d1cb074155fd54ea3107d8d787a32406ab68
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2022-11-01 23:22:28 +1100

    Authentication Client

--- client/src/redux/reducers/userReducer/types.js
+++ client/src/redux/reducers/userReducer/types.js
@@ -1,2 +1,3 @@
-export const GET_USER = 'GET_USER'
-export const DESTROY_USER = 'DESTROY_USER'
+export const REGISTER = "REGISTER";
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
--- server/src/controllers/authentication.js
+++ server/src/controllers/authentication.js
@@ -2,14 +2,14 @@ const bcrypt = require("bcrypt");
 const { User } = require("../db/models");
 
 exports.register = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { name, email, password } = req.body;
   const hash = await bcrypt.hash(password, 10);
-  const user = await User.create({ username, email, password: hash });
+  const user = await User.create({ name, email, password: hash });
   req.session.userId = user.id;
   req.session.save(() => {
     res.json({
       success: true,
-      user: { id: user.id, username: user.username, email: user.email },
+      user: { id: user.id, name: user.name, email: user.email },
     });
   });
 };
@@ -23,7 +23,7 @@ exports.login = async (req, res) => {
     req.session.save(() => {
       res.json({
         success: true,
-        user: { id: user.id, email: user.email, username: user.username },
+        user: { id: user.id, email: user.email, name: user.name },
       });
     });
   } else {
