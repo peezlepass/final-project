403abcadc3b6e8e46a5a675fa7de6506b0296555
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2022-11-03 02:01:18 +1100

    Extra changes

--- client/src/components/Minesweeper/Cell.jsx
+++ client/src/components/Minesweeper/Cell.jsx
@@ -1,3 +1,17 @@
-export default function Cell() {
-  return <div>Cell</div>;
+import BombGuess from "./Cells/BombGuess";
+import Unknown from "./Cells/Unknown";
+import Bomb from "./Cells/Bomb";
+import Empty from "./Cells/Empty";
+import Number from "./Cells/Number";
+
+const typeMap = {
+  unknown: () => <Unknown></Unknown>,
+  bomb: (props) => <Bomb {...props}></Bomb>,
+  bombGuess: (props) => <BombGuess {...props}></BombGuess>,
+  empty: () => <Empty></Empty>,
+  number: (props) => <Number {...props}></Number>,
+};
+
+export default function Cell({ type, props }) {
+  return typeMap[type](props);
 }
