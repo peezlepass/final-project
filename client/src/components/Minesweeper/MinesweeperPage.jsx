0a7b948feb90a1e8630b0079953fdd28d4c4199c
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2022-11-02 22:21:24 +1100

    Board is all together !!!

--- client/src/components/Minesweeper/MinesweeperPage.jsx
+++ client/src/components/Minesweeper/MinesweeperPage.jsx
@@ -14,27 +14,6 @@ export default function MinesweeperPage() {
   return (
     <div className="flex flex-col gap-y-8 items-center">
       <Board></Board>
-      <Header></Header>
-      <Restart mood="smile"></Restart>
-      <Restart mood="surprise"></Restart>
-      <Restart mood="dead"></Restart>
-      <Counter number={320}></Counter>
-      <Field></Field>
-      <Cell></Cell>
-      <Bomb></Bomb>
-      <Bomb red={true}></Bomb>
-      <BombGuess></BombGuess>
-      <BombGuess red={true}></BombGuess>
-      <Empty></Empty>
-      <Number number={1}></Number>
-      <Number number={2}></Number>
-      <Number number={3}></Number>
-      <Number number={4}></Number>
-      <Number number={5}></Number>
-      <Number number={6}></Number>
-      <Number number={7}></Number>
-      <Number number={8}></Number>
-      <Unknown></Unknown>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      borderWidth: {
+        6: "6px",
+      },
       colors: {
         "empty-cell-color": "rgb(186, 186, 186)",
         "empty-cell-border": "rgb(156, 156, 156)",
