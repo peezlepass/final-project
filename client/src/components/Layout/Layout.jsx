934eb884bb6c4c2a14d4b360e8306df8fada6940
Vadim Zhdanov <vadimir29@gmail.com>
2022-11-03 19:10:20 +0700

    Add modal reducer

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,11 +1,18 @@
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../redux/reducers/userReducer/actions";
+import { Modal } from "../Modal/Modal";
 
 export default function Layout({ children }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user.user);
+  const [modal, setModal] = React.useState({
+    type: "",
+    close: false,
+  });
+
   const handleLogout = async (e) => {
     e.preventDefault();
     const response = await fetch("/logout", {
@@ -19,6 +26,25 @@ export default function Layout({ children }) {
     dispatch(logout());
     navigate("/");
   };
+
+  const userLogin = () => {
+    setModal((prev) => {
+      return {
+        type: "login",
+        close: !prev.close,
+      };
+    });
+  };
+
+  const userRegister = () => {
+    setModal((prev) => {
+      return {
+        type: "registration",
+        close: !prev.close,
+      };
+    });
+  };
+
   return (
     <>
       <header>
@@ -32,13 +58,16 @@ export default function Layout({ children }) {
             </>
           ) : (
             <>
-              <Link to="/login">Login</Link>
-              <Link to="/register">Register</Link>
+              {/* <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link> */}
+              <div onClick={userLogin}>Login</div>
+              <div onClick={userRegister}>Register</div>
             </>
           )}
         </nav>
       </header>
       <div>{children}</div>
+      {modal.close ? <Modal setModal={setModal} modal={modal} /> : null}
     </>
   );
 }
--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
 import styles from "./Modal.module.css";
 
-export const Modal = () => {
+export const Modal = ({ setModal, modal }) => {
+  const closeModal = () => {
+    setModal((prev) => {
+      return {
+        type: "",
+        close: !prev.close,
+      };
+    });
+  };
   return (
     <>
       <div className={styles.overlay}>
         <div className={styles.modal}>
-          <button>close</button>
+          {modal.type === "registration" ? <Register /> : null}
+          {modal.type === "login" ? <Login /> : null}
+          <button onClick={closeModal}>close</button>
         </div>
       </div>
     </>
