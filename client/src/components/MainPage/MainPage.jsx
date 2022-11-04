71553ca169a2eb2c2a5b50bee2f9084fe564d1fe
Vadim Zhdanov <vadimir29@gmail.com>
2022-11-04 18:26:01 +0700

    Main styles pre-alpha

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -7,21 +7,23 @@ export const MainPage = () => {
     <div className={styles.wrapper}>
       <div className={styles.main}>
         <div className={styles.content}>
-          <Link to="/quiz" className={styles.game}>
-            Quiz
-          </Link>
-          <Link to="/snake" className={styles.game}>
-            Snake
-          </Link>
-          <Link to="/xox" className={styles.game}>
-            XOX
-          </Link>
-          <Link to="/minesweeper" className={styles.game}>
-            Minesweeper
-          </Link>
-          <Link to="/memorygame" className={styles.game}>
-            Memory Game
-          </Link>
+          <div className={styles.slider}>
+            <Link to="/quiz" className={styles.game}>
+              Quiz
+            </Link>
+            <Link to="/snake" className={styles.game}>
+              Snake
+            </Link>
+            <Link to="/xox" className={styles.game}>
+              XOX
+            </Link>
+            <Link to="/minesweeper" className={styles.game}>
+              Minesweeper
+            </Link>
+            <Link to="/memorygame" className={styles.game}>
+              Memory Game
+            </Link>
+          </div>
         </div>
       </div>
     </div>
