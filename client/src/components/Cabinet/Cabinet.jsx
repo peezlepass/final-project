a6d6fc431c458b9d7ac4e62fb5ca3ced7abad34c
Vadim Zhdanov <vadimir29@gmail.com>
2022-11-04 19:29:42 +0700

    Add simple styles to cabinet

--- client/src/components/Cabinet/Cabinet.jsx
+++ client/src/components/Cabinet/Cabinet.jsx
@@ -19,7 +19,33 @@ export const Cabinet = () => {
               </div>
             </div>
           </div>
-          <div className={styles.userInfo}></div>
+          <div className={styles.userInfo}>
+            <div className={styles.userAvatar}>
+              <div className={styles.avatar}></div>
+            </div>
+            <div className={styles.userStat}>
+              <div className={styles.userStatName}>
+                <div>Имя</div>
+                <div>{user.name}</div>
+              </div>
+              <div className={styles.userGender}>
+                <div>Пол</div>
+                <div>Мужской</div>
+              </div>
+              <div className={styles.userBirth}>
+                <div>День рождения</div>
+                <div>12.11.1991</div>
+              </div>
+              <div className={styles.userLocation}>
+                <div>Местонахлждение</div>
+                <div>Тайланд</div>
+              </div>
+              <div className={styles.userMeeting}>
+                <div>Присоединился</div>
+                <div>02.11.2022</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
