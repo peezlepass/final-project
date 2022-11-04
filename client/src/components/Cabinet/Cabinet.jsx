7b42f58f34cbb40415683ed5b139d65d3875daae
Vadim Zhdanov <vadimir29@gmail.com>
2022-11-04 19:09:04 +0700

    Start make cabinet styles

--- client/src/components/Cabinet/Cabinet.jsx
+++ client/src/components/Cabinet/Cabinet.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styles from "./cabinet.module.css";
 
 export const Cabinet = () => {
+  const user = useSelector((state) => state.user.user);
+  console.log(user);
   return (
     <>
-      <div>Cabinet</div>
+      <div className={styles.layout}>
+        <div className={styles.cabinet}>
+          <div className={styles.cabinetHeader}>
+            <div className={styles.userName}>
+              {user.name}
+              <div className={styles.userControls}>
+                <div>Профиль</div>
+                <div>Достижения</div>
+                <div>Таблица рекордов</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.userInfo}></div>
+        </div>
+      </div>
     </>
   );
 };
