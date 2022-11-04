eb5e4f642cc4ee509aa8b963a198f5877a4946e0
Vadim Zhdanov <vadimir29@gmail.com>
2022-11-04 20:02:23 +0700

    Add change page with btn in cabinet

--- client/src/components/Cabinet/Cabinet.jsx
+++ client/src/components/Cabinet/Cabinet.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import styles from "./cabinet.module.css";
+import { UserAchivment } from "./UserAchivment/UserAchivment";
+import { UserInfo } from "./UserInfo/UserInfo";
+import { UserRecords } from "./UserRecords/UserRecords";
 
 const activeBtn = {
   background: "black",
@@ -46,33 +49,9 @@ export const Cabinet = () => {
               </div>
             </div>
           </div>
-          <div className={styles.userInfo}>
-            <div className={styles.userAvatar}>
-              <div className={styles.avatar}></div>
-            </div>
-            <div className={styles.userStat}>
-              <div className={styles.userStatName}>
-                <div>Имя</div>
-                <div>{user.name}</div>
-              </div>
-              <div className={styles.userGender}>
-                <div>Пол</div>
-                <div>Мужской</div>
-              </div>
-              <div className={styles.userBirth}>
-                <div>День рождения</div>
-                <div>12.11.1991</div>
-              </div>
-              <div className={styles.userLocation}>
-                <div>Местонахлждение</div>
-                <div>Тайланд</div>
-              </div>
-              <div className={styles.userMeeting}>
-                <div>Присоединился</div>
-                <div>02.11.2022</div>
-              </div>
-            </div>
-          </div>
+          {select[1] ? <UserInfo user={user} /> : ""}
+          {select[2] ? <UserAchivment /> : ""}
+          {select[3] ? <UserRecords /> : ""}
         </div>
       </div>
     </>
---
+++ client/src/components/Cabinet/UserRecords/UserRecords.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./UserRecords.module.css";
+
+export const UserRecords = () => {
+  return <div>UserRecords</div>;
+};
