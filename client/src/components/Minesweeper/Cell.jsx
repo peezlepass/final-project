ca25c58047972a92b0518b1c4b06221d4e88af5c
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2022-11-05 20:39:30 +1100

    Red flags guessed correctly or incorrectly

--- client/src/components/Minesweeper/Cell.jsx
+++ client/src/components/Minesweeper/Cell.jsx
@@ -25,5 +25,9 @@ export default function Cell({ cell, dispatch, index }) {
     return <BombGuess dispatch={dispatch} index={index}></BombGuess>;
   }
 
+  if (cell === "GR") {
+    return <BombGuess dispatch={dispatch} index={index} red={true}></BombGuess>;
+  }
+
   return <Number number={cell}></Number>;
 }
