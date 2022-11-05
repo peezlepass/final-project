4c0952e2be6c29ced88e70cbf33d79329ad2babe
Vadim Zhdanov <vadimir29@gmail.com>
2022-11-05 14:45:25 +0700

    Add achive styles

--- client/src/components/Cabinet/UserAchivment/UserAchivment.jsx
+++ client/src/components/Cabinet/UserAchivment/UserAchivment.jsx
@@ -1,18 +1,37 @@
 import React from "react";
+import { OneAchiv } from "./OneAchiv/OneAchiv";
 import styles from "./UserAchivment.module.css";
 
+const achiveIcon = [
+  {
+    img: "https://static.stratege.ru/trophies/NPWR11536_00/TROP000_w100h100.PNG",
+  },
+  {
+    img: "https://static.stratege.ru/trophies/NPWR11536_00/TROP010_w100h100.PNG",
+  },
+  {
+    img: "https://static.stratege.ru/trophies/NPWR11536_00/TROP038_w100h100.PNG",
+  },
+  {
+    img: "https://static.stratege.ru/trophies/NPWR11536_00/TROP004_w100h100.PNG",
+  },
+  {
+    img: "https://static.stratege.ru/trophies/NPWR11536_00/TROP015_w100h100.PNG",
+  },
+  {
+    img: "https://static.stratege.ru/trophies/NPWR11536_00/TROP018_w100h100.PNG",
+  },
+  {
+    img: "https://static.stratege.ru/trophies/NPWR11536_00/TROP023_w100h100.PNG",
+  },
+];
 export const UserAchivment = () => {
   return (
     <>
       <div className={styles.userAchivment}>
-        <div className={styles.achive}>
-          <div className={styles.achiveImg}></div>
-          <div className={styles.achiveInfo}>
-            <div className={styles.achiveInfoTitle}></div>
-            <div className={styles.achiveInfoDesc}></div>
-            <div className={styles.achiveInfoScore}></div>
-          </div>
-        </div>
+        {achiveIcon.map((el, i) => (
+          <OneAchiv key={i} achiveIcon={achiveIcon} i={i} />
+        ))}
       </div>
     </>
   );
